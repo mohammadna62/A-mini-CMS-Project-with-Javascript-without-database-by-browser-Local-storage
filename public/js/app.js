995d1441a10f3da2d9248f86cee7b029f3c20b80
mohammad naghavi <mohammad.naghavi@gmail.com>
2026-02-17 22:15:19 +0330

    create user querySelectors,AddEventListener,Function and save data to object

---
+++ public/js/app.js
@@ -0,0 +1,99 @@
+const data = {
+  users: [
+    {
+      id: 1,
+      name: "پیمان احمدی",
+      username: "peyman",
+      email: "peyman@gmail.com",
+      password: "peyman1212",
+    },
+  ],
+
+  products: [
+    {
+      id: 1,
+      title: "کفش ورزشی",
+      price: 2000000,
+      slug: "nike-sport-shoe",
+    },
+  ],
+};
+//? <------------ User Selector Section ----------------->
+const createUserBtn = document.querySelector(".section-link");
+const userModal = document.querySelector("#user-modal-screen");
+const userFullName = document.querySelector("#user-fullName");
+const userUsername = document.querySelector("#user-username");
+const userEmail = document.querySelector("#user-email");
+const userPassword = document.querySelector("#user-password");
+const userCreateSubmitBtn = document.querySelector(".submit");
+const closeUserModalBtn = document.querySelector(".close-modal");
+const cancelUserBtn = document.querySelector(".cancel");
+
+//? <---------- Product Selector Section ---------------->
+
+//? <---------- Home Selector Section ---------------->
+const toggleMenu = document.querySelector(".toggle-sidebar");
+const homePageSidebar = document.querySelector(".sidebar");
+//! <----------- End Of Selector Section----------------->
+
+
+
+
+
+//? <------------ User Event Section ----------------->
+createUserBtn.addEventListener("click", ShowUserModal);
+userCreateSubmitBtn.addEventListener("click", createUser);
+closeUserModalBtn.addEventListener("click", hideUserModal);
+cancelUserBtn.addEventListener("click", hideUserModal);
+//*<---------------- Product Function Section ---------------->
+function addProductDataToLocalStorage(product) {
+  localStorage.setItem("user", product);
+}
+function getProductDataToLocalStorage(product) {
+  localStorage.getItem("product");
+}
+//*<---------------- Home Function Section ------------------->
+toggleMenu.addEventListener("click", function () {
+  homePageSidebar.classList.toggle("open");
+});
+//! <----------- End Of Function Section----------------->
+
+
+
+
+
+
+
+//*<---------------- User Function Section ------------------->
+function ShowUserModal() {
+  userModal.classList.remove("hidden");
+  userFullName.innerHTML = "";
+  userUsername.innerHTML = "";
+  userEmail.innerHTML = "";
+  userPassword.innerHTML = "";
+}
+function hideUserModal() {
+  userModal.classList.add("hidden");
+}
+function createUser() {
+  const userFullNameData = userFullName.value;
+  const userUsernameData = userUsername.value;
+  const userEmailData = userEmail.value
+  const userPasswordData = userPassword.value;
+  const newUser = {
+    id: Math.floor(Math.random() * 9999),
+    name: userFullNameData,
+    username: userUsernameData,
+    email: userEmailData,
+    password: userPasswordData,
+  };
+  data.users.push(newUser);
+  console.log(data.users);
+}
+function showUserData() {}
+function addUserDataToLocalStorage(user) {
+  localStorage.setItem("user", user);
+}
+function getUserDataToLocalStorage(user) {
+  localStorage.getItem("user");
+}
